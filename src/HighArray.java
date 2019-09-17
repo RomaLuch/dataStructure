a158409d036aab74677172e4c1467ead7ed4dfb2
roma <luchinsky@list.ru>
2019-09-17 17:11:18 +0300

    2.1 add getMax method

--- src/HighArray.java
+++ src/HighArray.java
@@ -3,77 +3,68 @@
 // Запуск программы: C>java HighArrayApp
 ////////////////////////////////////////////////////////////////
 class HighArray {
-    private long[] a;
-    // Ссылка на массив a
-    private int nElems;
+    private long[] a;    // Ссылка на массив a
+    private int nElems;    // Количество элементов в массиве
 
-    // Количество элементов в массиве
-//-----------------------------------------------------------
-    public HighArray(int max)
-// Конструктор
+    //-----------------------------------------------------------
+    public HighArray(int max) // Конструктор
     {
-        a = new long[max];
-// Создание массива
-        nElems = 0;
-// Пока нет ни одного элемента
+        a = new long[max]; // Создание массива
+        nElems = 0; // Пока нет ни одного элемента
     }
-//-----------------------------------------------------------Интерфейсы классов  
+//-----------------------------------------------------------Интерфейсы классов
 
     public boolean find(long searchKey) {
 // Поиск заданного значения
         int j;
-        for (j = 0; j < nElems; j++)
-// Для каждого элемента
-            if (a[j] == searchKey)
-// Значение найдено?
-                break;
-// Да - выход из цикла
-        if (j == nElems)
-// Достигнут последний элемент?
-            return false;
-// Да
-        else
-            return true;
-// Нет
+        for (j = 0; j < nElems; j++) // Для каждого элемента
+            if (a[j] == searchKey)// Значение найдено?
+                break;  // Да - выход из цикла
+        return j != nElems;
     }
 
     //-----------------------------------------------------------
-    public void insert(long value)
-// Вставка элемента в массив
+    public void insert(long value)// Вставка элемента в массив
     {
-        a[nElems] = value;
-// Собственно вставка
-        nElems++;
-// Увеличение размера
+        a[nElems] = value;// Собственно вставка
+        nElems++;// Увеличение размера
     }
 
     //-----------------------------------------------------------
     public boolean delete(long value) {
         int j;
-        for (j = 0; j < nElems; j++)
-// Поиск заданного значения
+        for (j = 0; j < nElems; j++)// Поиск заданного значения
             if (value == a[j])
                 break;
-        if (j == nElems)
-// Найти не удалось
+        if (j == nElems)// Найти не удалось
             return false;
-        else
-// Значение найдено
+        else// Значение найдено
         {
             for (int k = j; k < nElems; k++) // Сдвиг последующих элементов
                 a[k] = a[k + 1];
-            nElems--;
-// Уменьшение размера
+            nElems--;// Уменьшение размера
             return true;
         }
     }
 
+    public long getMax() {
+        if (nElems == 0) {
+            return -1;
+        }
+        long max = 0;
+
+        for (int i = 0; i < nElems; i++) {
+            if (a[i] > max) {
+                max = a[i];
+            }
+        }
+        return max;
+    }
+
     //-----------------------------------------------------------
-    public void display()
-// Вывод содержимого массива
+    public void display()// Вывод содержимого массива
     {
-        for (int j = 0; j < nElems; j++)
-// Для каждого элемента
+        for (int j = 0; j < nElems; j++)// Для каждого элемента
             System.out.print(a[j] + " "); // Вывод
         System.out.println("");
     }
--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         HighArray arr;
 // Ссылка на массив
         arr = new HighArray(maxSize); // Создание массива
+        System.out.println(arr.getMax());
         arr.insert(77);
         arr.insert(99);
         arr.insert(44);
@@ -14,7 +15,7 @@ public class Main {
         arr.insert(22);
         arr.insert(88);
         arr.insert(11);
-        arr.insert(00);
+        arr.insert(0);
         arr.insert(66);
         arr.insert(33); // Вставка 10 элементов
         arr.display(); // Вывод элементов
@@ -24,11 +25,16 @@ public class Main {
             System.out.println("Found " + searchKey);
         else
             System.out.println("Can't find " + searchKey);
-        arr.delete(00);
+
+        System.out.println(arr.getMax());
+
+        arr.delete(0);
         arr.delete(55);
         arr.delete(99);
 // Удаление трех элементов
         arr.display();
 // Повторный вывод
+
+        System.out.println(arr.getMax());
     }
 }
